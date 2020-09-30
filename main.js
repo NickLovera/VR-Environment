24883e0aebdc87ba87d2e9cca4f18b915940a30d
NickLovera <60933011+NickLovera@users.noreply.github.com>
2020-09-30 11:42:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,13 @@ document.querySelector('#radio').addEventListener('mouseenter', function () {
 	radio.setAttribute('scale', '5 5 5.4');	
 });
 
-//scaledown when mouse is out off fireplace
+//scaledown when mouse is out off radio
 document.querySelector('#radio').addEventListener('mouseleave', function () {
 	radio.setAttribute('scale', '0.5 1 0.5');	
 });
 
 
-document.querySelector('#chair').addEventListener('mouseenter', function () {
-	chair.setAttribute('scale', '5.4 5.4 5.4');	
-});
-
-//turn on off music of radio
+//turn music on/off
 document.querySelector('#radio').addEventListener('click', function () {
 	var entity = document.querySelector('[sound]');
 	
