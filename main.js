84fb23d970fde5404502dc29719f73194246da2b
NickLovera <60933011+NickLovera@users.noreply.github.com>
2020-09-30 14:01:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,27 @@ document.querySelector('#radio').addEventListener('mouseleave', function () {
 	radio.setAttribute('scale', '1 1 1');	
 });
 
+//Scale up button
+document.querySelector('#button').addEventListener('mouseenter', function () {
+	button.setAttribute('color', red);
+	button.setAttribute('scale', '1.5 1.5 1.5');	
+});
+
+//scaledown button
+document.querySelector('#button').addEventListener('mouseleave', function () {
+	button.setAttribute('color', green);
+	button.setAttribute('scale', '1 1 1');	
+});
+
+//Change to Covid mode
+document.querySelector('#button').addEventListener('click', function () {
+	if(button.components.color == green){
+		//switch to covid
+	}
+	else{
+		//switch back to normal
+	}
+});
 
 //turn music on/off
 document.querySelector('#radio').addEventListener('click', function () {
